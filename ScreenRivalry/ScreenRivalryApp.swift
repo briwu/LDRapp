70329477b00e3f119395d9f6441f01c3359cc966
Brian Wu <briawu@umich.edu>
2024-07-01 00:38:11 -0400

    refactor repo

---
+++ ScreenRivalry/ScreenRivalryApp.swift
@@ -0,0 +1,51 @@
+//
+//  ScreenRivalryApp.swift
+//  ScreenRivalry
+//
+//  Created by Brian Wu on 7/1/24.
+//
+
+import SwiftUI
+
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
+
+
+@main
+
+struct ScreenRivalryApp: App {
+
+  // register app delegate for Firebase setup
+
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+
+    WindowGroup {
+
+      NavigationView {
+
+        ContentView()
+
+      }
+
+    }
+
+  }
+
+}
