624ad92b6f0087e53a6676329ee2ed00b5c463af
Brian Wu <briawu@umich.edu>
2024-07-07 01:09:32 -0400

    implement scene phase

--- ScreenRivalry/Core/StartPage/IndividualSessionView.swift
+++ ScreenRivalry/Core/StartPage/IndividualSessionView.swift
@@ -6,10 +6,39 @@
 //
 
 import SwiftUI
+import Combine
+
+class AppStateViewModel: ObservableObject {
+    @Published var isInForeground: Bool = true
+}
+
 
 struct IndividualSessionView: View {
+    @Environment(\.scenePhase) var scenePhase
+    @StateObject private var appStateViewModel = AppStateViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if appStateViewModel.isInForeground {
+                Text("The app is in the foreground.")
+            } else {
+                Text("The app is in the background.")
+            }
+        }
+        .onChange(of: scenePhase) {
+            switch scenePhase {
+            case .active:
+                appStateViewModel.isInForeground = true
+                print("App is in the foreground.")
+            case .inactive:
+                appStateViewModel.isInForeground = false
+                print("App is inactive.")
+            case .background:
+                appStateViewModel.isInForeground = false
+                print("App is in the background.")
+            @unknown default:
+                break
+            }
+        }
     }
 }
 
