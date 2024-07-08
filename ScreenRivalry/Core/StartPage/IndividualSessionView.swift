dfdfb4f2ef5e19a06583dd82e719e6d5c00595be
Brian Wu <briawu@umich.edu>
2024-07-08 00:35:58 -0400

    progress on timer

--- ScreenRivalry/Core/StartPage/IndividualSessionView.swift
+++ ScreenRivalry/Core/StartPage/IndividualSessionView.swift
@@ -12,32 +12,106 @@ class AppStateViewModel: ObservableObject {
     @Published var isInForeground: Bool = true
 }
 
+class TimerViewModel: ObservableObject {
+    @Published var timerValue: Int = 0
+    var countdownDuration: Int = 0
+    var timer: Timer?
+    
+    init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAppEnteringBackground), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAppEnteringForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func startTimer() {
+        guard timer == nil else { return } // Prevent starting multiple timers
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            if self.timerValue > 0 {
+                self.timerValue -= 1
+            } else {
+                self.stopTimer()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func toggleTimer() {
+        if timer == nil {
+            startTimer()
+        } else {
+            stopTimer()
+        }
+    }
+    
+    func setCountdown(duration: Int) {
+        countdownDuration = duration
+        timerValue = duration
+    }
+    
+    @objc func handleAppEnteringBackground() {
+        stopTimer()
+    }
+    
+    @objc func handleAppEnteringForeground() {
+        startTimer()
+    }
+}
+
 
 struct IndividualSessionView: View {
-    @Environment(\.scenePhase) var scenePhase
-    @StateObject private var appStateViewModel = AppStateViewModel()
+    @Environment(\.scenePhase) private var scenePhase
+    @StateObject private var timerViewModel = TimerViewModel()
+    @State private var selectedDurationIndex = 0
+    let countdownDurations = [5, 10, 15, 20, 25]
+    
     var body: some View {
         VStack {
-            if appStateViewModel.isInForeground {
-                Text("The app is in the foreground.")
-            } else {
-                Text("The app is in the background.")
+            Picker(selection: $selectedDurationIndex, label: Text("Select Duration")) {
+                ForEach(0..<countdownDurations.count, id: \.self) { index in
+                    Text("\(countdownDurations[index]) seconds")
+                }
             }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            Text("Individual Session Timer: \(timerViewModel.timerValue)")
+                .font(.largeTitle)
+                .padding()
+
+            Button(action: {
+                let selectedDuration = countdownDurations[selectedDurationIndex]
+                timerViewModel.setCountdown(duration: selectedDuration)
+                timerViewModel.toggleTimer()
+            }) {
+                Text(timerViewModel.timer == nil ? "Start Timer" : "Stop Timer")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+            }
+        }
+        .onChange(of: selectedDurationIndex) {
+            let selectedDuration = countdownDurations[selectedDurationIndex]
+            timerViewModel.setCountdown(duration: selectedDuration)
         }
         .onChange(of: scenePhase) {
             switch scenePhase {
             case .active:
-                appStateViewModel.isInForeground = true
-                print("App is in the foreground.")
-            case .inactive:
-                appStateViewModel.isInForeground = false
-                print("App is inactive.")
-            case .background:
-                appStateViewModel.isInForeground = false
-                print("App is in the background.")
+                break
+            case .background, .inactive:
+                timerViewModel.stopTimer()
             @unknown default:
                 break
             }
+        }
+        .onAppear {
         }
     }
 }
